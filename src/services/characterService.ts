9ef199668a955ed5d8431b2141e5ed08d0799828
Tristan Leonard <tristanl@slalom.com>
2019-04-23 07:17:50 -0700

    Changing character service to rely directly on the blizzy service instead of another endpoint.

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -1,31 +1,18 @@
-import Axios from "axios";
 import { BasicResponse } from "../types/basicResponse";
+import { BlizzyService } from "./blizzyService";
 
 export interface CharacterServiceProtocol {
     getCharacterInfo(token: string, characterName: string, serverName: string): Promise<BasicResponse>
 }
 
 export class CharacterService implements CharacterServiceProtocol {
-    constructor(private environment: { baseUrl: string }) { }
+    constructor(private blizzyService: BlizzyService) { }
 
     async getCharacterInfo(token: string, characterName: string, serverName: string): Promise<BasicResponse> {
         try {
-            const params = { token: token, resource: `wow/character/${serverName}/${characterName}`, params: {} }
-            const options = {
-                baseURL: this.environment.baseUrl,
-                params: params
-            }
-            const result = await Axios.post('service', params, options)
-            console.log(result.data)
-            const response: BasicResponse = {
-                statusCode: result.status,
-                body: JSON.stringify(result.data),
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': true,
-                }
-            }
-            return response
+            const result = await this.blizzyService.execute(token, `wow/character/${serverName}/${characterName}`, {})
+            console.log(result.body)
+            return result
         } catch (error) {
             const response = error.response
             const responseText = response.status == 401 ? "Unauthorized. Check that your authorization token is still valid." : response.statusText
